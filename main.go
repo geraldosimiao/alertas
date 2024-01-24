09a79d619973f84f3ce0c4453bb499c5cbcdb71c
geraldosimiao <72357821+geraldosimiao@users.noreply.github.com>
2024-01-24 14:26:16 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,88 +1,28 @@
 package main
 
 import (
-    "bytes"
-    "encoding/xml"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "strings"
-    "golang.org/x/net/html"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
 )
 
 // Aviso representa a estrutura de dados para um aviso meteorológico
 type Aviso struct {
-    Title       string `xml:"title"`
-    Link        string `xml:"link"`
-    Description string `xml:"description"`
-    Published   string `xml:"pubDate"`
-}
-
-func extrairCampos(aviso *Aviso) error {
-    // Parse a descrição como HTML
-    descHTML, err := html.Parse(strings.NewReader(aviso.Description))
-    if err != nil {
-        return err
-    }
-
-    // Encontrar todas as células da tabela
-    cells := findTableCells(descHTML)
-
-    // Atribuir valores aos campos correspondentes
-    if len(cells) >= 8 {
-        aviso.Status = cells[0]
-        aviso.Evento = cells[1]
-        aviso.Severidade = cells[2]
-        aviso.Início = cells[3]
-        aviso.Fim = cells[4]
-        aviso.Descrição = cells[5]
-        aviso.Área = cells[6]
-        aviso.LinkGráfico = cells[7]
-    }
-
-    return nil
-}
-
-func findTableCells(n *html.Node) []string {
-    var cells []string
-
-    var f func(*html.Node)
-    f = func(n *html.Node) {
-        if n.Type == html.ElementNode && n.Data == "td" {
-            cells = append(cells, textContent(n))
-        }
-        for c := n.FirstChild; c != nil; c = c.NextSibling {
-            f(c)
-        }
-    }
-
-    f(n)
-
-    return cells
-}
-
-func textContent(n *html.Node) string {
-    var buffer bytes.Buffer
-    var f func(*html.Node)
-    f = func(n *html.Node) {
-        if n.Type == html.TextNode {
-            buffer.WriteString(n.Data)
-        }
-        for c := n.FirstChild; c != nil; c = c.NextSibling {
-            f(c)
-        }
-    }
-    f(n)
-    return buffer.String()
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	Published   string `xml:"pubDate"`
 }
 
 // Feed representa a estrutura de dados para um feed RSS
 type Feed struct {
 	Channel struct {
-		Title       string `xml:"title"`
-		Link        string `xml:"link"`
-		Description string `xml:"description"`
+		Title       string  `xml:"title"`
+		Link        string  `xml:"link"`
+		Description string  `xml:"description"`
 		Items       []Aviso `xml:"item"`
 	} `xml:"channel"`
 }
@@ -101,8 +41,7 @@ func obterAvisos(apiURL string) ([]Aviso, error) {
 		return nil, err
 	}
 
-	// Adicionar a linha abaixo para imprimir o XML completo
-	// fmt.Printf("Resposta da API:\n%s\n", conteúdo)
+	fmt.Printf("Resposta da API:\n%s\n", conteúdo)
 
 	var feed Feed
 	err = xml.Unmarshal(conteúdo, &feed)
@@ -129,12 +68,33 @@ func main() {
 		fmt.Printf("Título: %s\n", aviso.Title)
 		fmt.Printf("Link: %s\n", aviso.Link)
 		fmt.Printf("Data de Publicação: %s\n", aviso.Published)
-		fmt.Printf("Status: %s\n", aviso.Status)
-		fmt.Printf("Evento: %s\n", aviso.Evento)
-		fmt.Printf("Severidade: %s\n", aviso.Severidade)
-		fmt.Printf("Início: %s\n", aviso.Início)
-		fmt.Printf("Fim: %s\n", aviso.Fim)
-		fmt.Printf("Área: %s\n", aviso.Área)
+
+		// Agora, você precisa analisar o conteúdo HTML da descrição
+		// Aqui, estamos extraindo informações usando strings e HTML parsing
+		reader := strings.NewReader(aviso.Description)
+		tokenizer := html.NewTokenizer(reader)
+
+		for {
+			tokenType := tokenizer.Next()
+			switch tokenType {
+			case html.ErrorToken:
+				return
+			case html.StartTagToken, html.SelfClosingTagToken:
+				token := tokenizer.Token()
+				if token.Data == "th" {
+					tokenType = tokenizer.Next()
+					if tokenType == html.TextToken {
+						fmt.Printf("%s: ", token.Data)
+						fmt.Println(tokenizer.Token().Data)
+					}
+				} else if token.Data == "td" {
+					tokenType = tokenizer.Next()
+					if tokenType == html.TextToken {
+						fmt.Printf("%s\n", tokenizer.Token().Data)
+					}
+				}
+			}
+		}
 		fmt.Println("-----")
 	}
 }
